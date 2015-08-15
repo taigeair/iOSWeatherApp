628149adc02d5ad8526fa93555c055516761a60c
taigeair <taigezhang@AirShow.local>
2015-08-16 00:46:48 +0200

    working version of weather app

---
+++ Weather2/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  Weather2
+//
+//  Created by Taige Zhang on 15/08/2015.
+//  Copyright (c) 2015 Taige Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var responseWeather: UILabel!
+    
+    @IBOutlet weak var userCity: UITextField!
+    
+    var weatherReport = ""
+    
+    func showError(){
+        if userCity.text == nil{
+            responseWeather.text = "Please enter a city."
+        } else {
+        responseWeather.text = "Was not able to find weather for " + userCity.text + ". Please retry."
+        }
+    }
+    
+
+    @IBAction func getWeather(sender: AnyObject) {
+        
+        self.view.endEditing(true)
+        
+        var cleanString = userCity.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/" + cleanString.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if url != nil{
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data,response,error) -> Void in
+                
+                var urlError = false
+                
+                if error == nil {
+                    
+                    var urlContent = NSString(data: data , encoding: NSUTF8StringEncoding) as NSString!
+                    var weather = urlContent.componentsSeparatedByString("<span class=\"phrase\">") as NSArray
+                    
+                    if weather.count > 1{
+                        var weatherPhrase = weather[1].componentsSeparatedByString("</span>")
+                        self.weatherReport = weatherPhrase[0] as! String
+                        
+                        self.weatherReport = self.weatherReport.stringByReplacingOccurrencesOfString("&deg;", withString: "˚")
+                        print(self.weatherReport)
+                    } else {
+                        urlError = true
+                    }
+                    
+                    
+                } else {
+                    urlError = true
+                    println("error")
+                }
+                
+                dispatch_async(dispatch_get_main_queue()){
+                    if urlError == true{
+                        self.showError()
+                    } else {
+                        self.responseWeather.text = self.weatherReport
+                    }
+                    
+                }
+            })
+            
+            task.resume()
+            
+        } else {
+            
+            showError()
+            
+        }
+
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       self.userCity.delegate = self
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        getWeather(self)
+        return true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+        
+    }
+    
+}
+
